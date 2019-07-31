3e64c1529458c9361a19c8746b805103e6e12ab4
Stan1slaw <stas.dudartchik@mail.ru>
2019-07-31 13:34:36 +0300

    change password

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,4 +82,4 @@ Comment.create!([
   {body: "nice film!!!!!!!!!!!!!!!!!!!!!!!!!!1", commentable_type: "Film", commentable_id: 1, user_id: 1},
   {body: "i think to", commentable_type: "Comment", commentable_id: 1, user_id: 1}
 ])
-AdminUser.create!(email: 'admin@gmail.com', password: 'nopassword', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@gmail.com', password: 'nopassword', password_confirmation: 'nopassword') if Rails.env.development?
