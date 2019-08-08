20a3198c9b070346015de7bf84e2d2d6c4d56ae5
stanislaw <stas.dudartchik@mail.ru>
2019-08-08 14:36:13 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_133951) do
+ActiveRecord::Schema.define(version: 2019_08_08_134747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,15 @@ ActiveRecord::Schema.define(version: 2019_08_08_133951) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.string "favorites_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -185,6 +194,7 @@ ActiveRecord::Schema.define(version: 2019_08_08_133951) do
 
   add_foreign_key "actors_films", "actors"
   add_foreign_key "actors_films", "films"
+  add_foreign_key "albums", "users"
   add_foreign_key "favorites", "films"
   add_foreign_key "favorites", "users"
   add_foreign_key "films", "franchises"
