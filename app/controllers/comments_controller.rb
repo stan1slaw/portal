18444163bb325a3bd91729b87d3cbe58a1caf934
Stan1slaw <stas.dudartchik@mail.ru>
2019-08-08 10:34:30 +0300

    add user_id to controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @commentable.comments.new(comment_params)
-
+    @comment.user_id = current_user.id if current_user
     if @comment.save
       redirect_to films_path, :notice => 'Comment created!'
     else
