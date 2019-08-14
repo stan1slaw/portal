f4c1a05546bc34e75a43eee675c78be73638294f
Stan1slaw <stas.dudartchik@mail.ru>
2019-08-14 12:53:02 +0300

    small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
   end
 end
--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,10 +1,5 @@
 module ImageHelper
     def picturevalid(url)
-    
-        if url.nil?
-            url = "https://cdn.iconscout.com/icon/premium/png-256-thumb/human-user-profile-man-face-avatar-51412.png"
-        else
-        url
-        end
+         url.nil? ? url = "https://cdn.iconscout.com/icon/premium/png-256-thumb/human-user-profile-man-face-avatar-51412.png" : url
     end
 end
